85a92dbdb312d377cdf364007706438589f219b0
eulbyvan <work.issdq@gmail.com>
2023-05-08 11:33:00 +0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eulbyvan/go-enigma-laundry
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+ * Author : Ismail Ash Shidiq (https://eulbyvan.netlify.app)
+ * Created on : Mon May 08 2023 11:32:12 AM
+ * Copyright : Ismail Ash Shidiq © 2023. All rights reserved
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+}
